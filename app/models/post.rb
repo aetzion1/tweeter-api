3ed2aba66f13709577e2f3d3d93459df5529bc3b
Adam Etzion <adametzion90@gmail.com>
2021-04-11 17:45:53 -0600

    Add posts index and show methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  validates :user, presence: true
+  # validates :user, presence: true
   validates :text, presence: true
   validates :likes, presence: true
   validates :repost_count, presence: true
+
+  scope :sorted, -> { order(created_at: :desc) }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :password, presence: true
 
   has_secure_password
